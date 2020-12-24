905dd55a76b76574ec13d37914e28cdc1d4d4e09
ppkhairn <ppkhairn@mtu.edu>
2020-12-23 19:19:11 -0500

    Pushkar merge sql_alchemy install, script changes

--- launch_noTPU.sh
+++ launch_noTPU.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+#!/bin/sh
 
 cd /home/pi/SensorFusion/
 
 source SF-env/bin/activate
 
-cd /home/pi/SensorFusion/Demo90
+cd /home/pi/SensorFusion/Demo90/
 
-python3 TFLite_detection_webcam_api.py --modeldir=Sample_TFLite_model |& python3 -mwebbrowser http://localhost:5000
+python3 -mwebbrowser http://localhost:5000 |& python3 TFLite_detection_webcam_api.py --modeldir=Sample_TFLite_model 
